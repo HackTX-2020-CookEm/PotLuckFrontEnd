aa0e4d3ec88b1f8058ffa188e18dc65acecdf7c0
Eric Wang <ericrwang2011@gmail.com>
2020-10-25 02:07:31 -0500

    fixed recommendation list

--- screens/SearchableRestaurants.js
+++ screens/SearchableRestaurants.js
@@ -85,76 +85,76 @@ class FlatListDemo extends Component {
       };
   
       this.arrayholder = [
-            {
-                name:{first:'Ramen Tatsu-Ya', last: 'Japanese'},
-                user:'1234 S Lamar Blvd, Austin, TX 78704',
-                picture:'https://i.pinimg.com/originals/7e/7e/93/7e7e938e9821be8c99c63fca9a2f068f.png',
-            },
-            {
-                name:{first:'Loro', last: 'Asian-American'},
-                user:'2115 S Lamar Blvd, Austin, TX 78704',
-                picture:'https://s3.amazonaws.com/cuag/entities/000/131/700/avatar180-240f1790603f67d52b442a02d5d57c80-110-137-816-736.jpg',
-            },
-            {
-                name:{first:'Kokodak', last: 'Korean'},
-                user:'2522 Guadalupe St, Austin, TX 78705',
-                picture:'https://pbs.twimg.com/profile_images/2217729421/kokodak_new_400x400.jpg',
-            },
-            {
-                name:{first:"Gong cha"},
-                user:'2021 Guadalupe St, Austin, TX 78705',
-                picture:'https://seeklogo.com/images/G/gongcha-logo-40F7622035-seeklogo.com.png',
-            },
-            {
-                name:{first:'Snooze, an A.M. Eatery', last: 'American'},
-                user:'3800 N Lamar Blvd Suite 120, Austin, TX 78756',
-                picture:'https://www.snoozeeatery.com/wp-content/uploads/2019/10/Logo.jpg',
-            },
-            {
-                name:{first:"Hopdoddy Burger Bar", last: 'American'},
-                user:'4615 N Lamar Blvd Suite 307, Austin, TX 78751',
-                picture:'https://i.pinimg.com/originals/a6/d0/cf/a6d0cf480d58e252fda2a9f23d64ff0d.png',
-            },
-            {
-                name:{first:"Kung Fu Tea"},
-                user:'2001 Guadalupe St #A2, Austin, TX 78705',
-                picture:'https://s.hdnux.com/photos/01/05/34/73/18206424/5/920x920.jpg',
-            },
-            {
-                name:{first:'Sharetea'},
-                user:'13343 US-183 #275, Austin, TX 78750',
-                picture:'https://www.capitaland.com/content/dam/capitaland-tenants/imported/en/-/media/cma-malls/websites/storefront_560/s/sharetea_560.jpg.transform/cap-lowres/image.jpg',
-            },
-            {
-                name:{first:'Roaring Fork', last: 'American'},
-                user:'701 Congress Ave, Austin, TX 78701',
-                picture:'https://arizonarestaurantweek.com/wp-content/uploads/2019/08/roaring-fork-logo.jpg',
-            },
-            {
-                name:{first:'Sway', last: 'Thai'},
-                user:'11501 Rock Rose Ave #100, Austin, TX 78758',
-                picture:'https://s3.amazonaws.com/toasttab/restaurants/restaurant-54416000000000000/restaurant_1593444511_180.png',
-            },
-            {
-                name:{first:'Frost Gelato', last: 'Dessert'},
-                user:'11700 Rock Rose Ave suite 176 suite 176, Austin, TX 78758',
-                picture:'https://gpsvnet.net/wp-content/uploads/2017/05/frost.jpeg',
-            },
-            {
-                name:{first:'JINYA Ramen Bar', last: 'Japanese'},
-                user:'3210 Esperanza Crossing Suite 130, Austin, TX 78758',
-                picture:'https://pbs.twimg.com/profile_images/916114096742793216/3VVRQT9R.jpg',
-            },
-            {
-                name:{first:'Culinary Dropout', last: 'American'},
-                user:'11721 Rock Rose Ave Ste #100, Austin, TX 78758',
-                picture:'https://2.bp.blogspot.com/-1-mRiBiJdfc/UKGM5b3SRcI/AAAAAAAABqw/oEq0dufAJQU/s1600/culinarydropout-logo.jpeg',
-            },
-            {
-                name:{first:'SUSHI JUNAI 1', last: 'Japanese'},
-                user:'1612 Lavaca St, Austin, TX 78701',
-                picture:'https://media.glassdoor.com/sqll/3005343/sushi-junai-omakase-squarelogo-1567659183552.png',
-            },
+        {
+          name:{first:'Ramen Tatsu-Ya', last: 'Japanese'},
+          user:'1234 S Lamar Blvd, Austin, TX 78704',
+          picture:'https://i.pinimg.com/originals/7e/7e/93/7e7e938e9821be8c99c63fca9a2f068f.png',
+      },
+      {
+          name:{first:'Loro', last: 'Asian-American'},
+          user:'2115 S Lamar Blvd, Austin, TX 78704',
+          picture:'https://s3.amazonaws.com/cuag/entities/000/131/700/avatar180-240f1790603f67d52b442a02d5d57c80-110-137-816-736.jpg',
+      },
+      {
+          name:{first:'Kokodak', last: 'Korean'},
+          user:'2522 Guadalupe St, Austin, TX 78705',
+          picture:'https://pbs.twimg.com/profile_images/2217729421/kokodak_new_400x400.jpg',
+      },
+      {
+          name:{first:"Siena Restaurant", last: 'Italian'},
+          user:'6203 N Capital of Texas Hwy, Austin, TX 78731',
+          picture:'https://sienaaustin.com/wp-content/uploads/2019/02/Siena-Toscana-logo-2x.png',
+      },
+      {
+          name:{first:'Snooze, an A.M. Eatery', last: 'American'},
+          user:'3800 N Lamar Blvd Suite 120, Austin, TX 78756',
+          picture:'https://www.snoozeeatery.com/wp-content/uploads/2019/10/Logo.jpg',
+      },
+      {
+          name:{first:"Hopdoddy Burger Bar", last: 'American'},
+          user:'4615 N Lamar Blvd Suite 307, Austin, TX 78751',
+          picture:'https://i.pinimg.com/originals/a6/d0/cf/a6d0cf480d58e252fda2a9f23d64ff0d.png',
+      },
+      {
+          name:{first:"Z'Tejas", last: 'Mexican'},
+          user:'9400 A, Arboretum Blvd, Austin, TX 78759',
+          picture:'https://www.24-7pressrelease.com/assets/attachments/046/press_release_distribution_0464922_142950.jpg',
+      },
+      {
+          name:{first:'North Italia', last: 'Italian'},
+          user:'11506 Century Oaks Terrace Suite 124, Austin, TX 78758',
+          picture:'https://pinpointretail.com/wp-content/uploads/2017/05/projects-northitalia.png',
+      },
+      {
+          name:{first:'Roaring Fork', last: 'American'},
+          user:'701 Congress Ave, Austin, TX 78701',
+          picture:'https://arizonarestaurantweek.com/wp-content/uploads/2019/08/roaring-fork-logo.jpg',
+      },
+      {
+          name:{first:'Sway', last: 'Thai'},
+          user:'11501 Rock Rose Ave #100, Austin, TX 78758',
+          picture:'https://s3.amazonaws.com/toasttab/restaurants/restaurant-54416000000000000/restaurant_1593444511_180.png',
+      },
+      {
+          name:{first:'Frost Gelato', last: 'Dessert'},
+          user:'11700 Rock Rose Ave suite 176 suite 176, Austin, TX 78758',
+          picture:'https://gpsvnet.net/wp-content/uploads/2017/05/frost.jpeg',
+      },
+      {
+          name:{first:'JINYA Ramen Bar', last: 'Japanese'},
+          user:'3210 Esperanza Crossing Suite 130, Austin, TX 78758',
+          picture:'https://pbs.twimg.com/profile_images/916114096742793216/3VVRQT9R.jpg',
+      },
+      {
+          name:{first:'Culinary Dropout', last: 'American'},
+          user:'11721 Rock Rose Ave Ste #100, Austin, TX 78758',
+          picture:'https://2.bp.blogspot.com/-1-mRiBiJdfc/UKGM5b3SRcI/AAAAAAAABqw/oEq0dufAJQU/s1600/culinarydropout-logo.jpeg',
+      },
+      {
+          name:{first:'SUSHI JUNAI 1', last: 'Japanese'},
+          user:'1612 Lavaca St, Austin, TX 78701',
+          picture:'https://media.glassdoor.com/sqll/3005343/sushi-junai-omakase-squarelogo-1567659183552.png',
+      },
         ];
     }
   
